8ed0245c85a295dcddf9c50bc887e27f09e326ea
Seungki Kim <seungki.kim@vtouch.io>
2020-05-23 17:37:29 +0900

    Refactor models (Teacher/Student)

---
+++ models.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Teacher(nn.Module):
+    def __init__(self):
+        # python requires to call ancestor's initilizer manually!
+        super(Teacher, self).__init__()
+        self.layer1 = nn.Linear(784, 1200)
+        self.layer2 = nn.Linear(1200, 1200)
+        self.layer3 = nn.Linear(1200, 10)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+class Student(nn.Module):
+    def __init__(self):
+        # python requires to call ancestor's initilizer manually!
+        super(Student, self).__init__()
+        self.layer1 = nn.Linear(784, 800)
+        self.layer2 = nn.Linear(800, 800)
+        self.layer3 = nn.Linear(800, 10)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
